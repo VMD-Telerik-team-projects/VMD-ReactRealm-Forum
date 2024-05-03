694d634c71b6ce3778eae6b078694c206ef34ad7
DeyanTomov <deyan.k.tomov@gmail.com>
2024-05-03 12:45:25 +0300

    Basic home page

---
+++ vite-project/src/components/Navigation/Navigation.jsx
@@ -0,0 +1,31 @@
+import { Navbar, Button } from 'react-bootstrap';
+
+
+export default function Navigation() {
+    return (
+      <Navbar expand="md" bg="dark" variant="dark" className="py-3" style={{ '--bs-body-bg': '#000000' }}>
+        <Navbar.Brand href="#" className="d-flex align-items-center me-4">
+          <img src="assets/img/logo-no-background.png" width="190px" className="ms-4" alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navcol-1" className="text-bg-light">
+          <span className="visually-hidden">Toggle navigation</span>
+          <span className="navbar-toggler-icon" style={{ color: 'var(--bs-secondary-bg)' }}></span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navcol-1">
+          <Nav className="me-auto">
+            <Nav.Link href="#" className="active fs-5 link-light me-3 ms-4" style={{ fontSize: '20px' }}>Home</Nav.Link>
+            <Nav.Link href="#" className="fs-5 link-light me-3" style={{ fontSize: '20px' }}>Posts</Nav.Link>
+            <Nav.Link href="#" className="fs-5 link-light" style={{ fontSize: '20px' }}>About</Nav.Link>
+          </Nav>
+          <Nav className="d-flex flex-row flex-wrap">
+            <Nav.Item className="d-flex me-4" style={{ marginRight: '0px' }}>
+              <Button variant="link" className="link-light me-0" style={{ border: '2px solid var(--bs-secondary-bg)' }}>Sign In</Button>
+            </Nav.Item>
+            <Nav.Item>
+              <Button variant="light" className="me-4">Sign Up</Button>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
---
+++ vite-project/src/views/Home/Home.jsx
@@ -0,0 +1,14 @@
+import {Button} from 'react-bootstrap'
+
+export default function MainContainer() {
+    return (
+      <div className="container d-flex flex-column justify-content-xl-center align-items-xl-center justify-content-xxl-center align-items-xxl-center main-container">
+        <h1 className="fs-1 mb-5" style={{ color: 'rgb(33,37,41)' }}>Sign in or register to access all posts</h1>
+        <div>
+          <Button variant="lg" className="me-4" style={{ border: '1px solid #000000' }}>Sign In</Button>
+          <Button variant="lg" style={{ color: 'rgb(255,255,255)', background: '#000000', border: '2px solid #000000' }}>Sign Up</Button>
+        </div>
+        <h5 className="fw-bold mt-4" style={{ color: 'rgba(33,37,41,0.4)' }}>or browse our top posts anonymously</h5>
+      </div>
+    );
+  }
