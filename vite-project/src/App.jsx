05eb7bd895600bec8f90bc54143f1d4b837bd012
MiroslavDermendzhiev <miroslav.der78@gmail.com>
2024-05-03 13:04:03 +0300

    Nav imported to Navigation component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './components/Navigation/Navigation';
-import MainContainer from './views/Home/Home';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navigation from "./components/Navigation/Navigation";
+import MainContainer from "./views/Home/Home";
 
 function App() {
   return (
-   <>
-    <Navigation />
-    <MainContainer />
-   </>
-  )
+    <>
+      <Navigation />
+      <MainContainer />
+    </>
+  );
 }
 
-export default App
+export default App;
