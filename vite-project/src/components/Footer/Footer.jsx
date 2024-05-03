66444925ab76c6fbe360ae41eb9bccb641b4cca0
DeyanTomov <deyan.k.tomov@gmail.com>
2024-05-03 15:02:18 +0300

    Fixed layout (centered main element)

--- vite-project/src/components/Footer/Footer.jsx
+++ vite-project/src/components/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 import CIcon from '@coreui/icons-react';
-import { cibGithub, cibGmail } from '@coreui/icons';
+import { cibGithub, cibGmail, cibFacebook } from '@coreui/icons';
 import './Footer.css'
 
 export default function Footer() {
   //  TODO: Replace <p> placeholders with icons
+  //  TODO: Fix icons
+
   return (
     <div
-      className="d-flex flex-nowrap w-full flex-row justify-content-center dark text-white gap-5 mt-auto py-3 fs-5 fw-light border border-3"
-      variant="dark"
-    >
+      className="d-flex flex-nowrap w-full flex-row justify-content-center text-white gap-5 mt-auto py-3 fs-5 fw-light border border-3">
       <CIcon icon={cibGithub} className='icon-styles'/>
       <CIcon icon={cibGmail} className='icon-styles'/>
-      <span>More</span>
+      <CIcon icon={cibFacebook} className='icon-styles'/>
     </div>
   )
 }
--- vite-project/src/hoc/Layout.jsx
+++ vite-project/src/hoc/Layout.jsx
@@ -1,16 +1,17 @@
 import Navigation from '../components/Navigation/Navigation'
 import Footer from '../components/Footer/Footer'
+import './Layout.css'
 
 export default function Layout({ children }) {
   //  TODO: Create footer and stick under main section
-  
+
   return (
-  <div className='d-flex flex-column justify-content-center min-vh-100'>
-    <Navigation />
-    <main className='d-flex flex-column align-self-center h-full'>
-      {children}
-    </main>
-    <Footer />
-  </div>
+    <div className='d-flex flex-column min-vh-100'>
+      <Navigation />
+      <main className='d-flex flex-column h-full flex-grow d-flex justify-content-center align-items-center'>
+        {children}
+      </main>
+      <Footer />
+    </div>
   )
 }
