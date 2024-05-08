09ba0b9cee577871b4cbaff0b5bbe6f4ebee9023
Vladislava-Ivanova <ivanovavladislavav@gmail.com>
2024-05-08 13:07:10 +0300

    search bar component style - primary

--- vite-project/src/components/SearchBar/SearchBar.jsx
+++ vite-project/src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,17 @@
 export default function SearchBar({ value, onChange }) {
   return (
-    <input
-      type="text"
-      placeholder="Search here"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <div className="input-group" style={{ maxWidth: "700px" }}>
+      <input
+        type="search"
+        className="form-control rounded"
+        placeholder="Search"
+        aria-label="Search"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+      <button type="button" className="btn btn-primary btn-floating" data-mdb-ripple-init>
+        Search
+      </button>
+    </div>
   );
 }
