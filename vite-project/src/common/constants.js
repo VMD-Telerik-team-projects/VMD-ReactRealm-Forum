0eb83f3955c8de89d53474c11073100639bcf771
deyantomov <deyan.tomov.a58@learn.telerikacademy.com>
2024-05-08 10:54:03 +0300

    Added title and content limits to create new post

--- vite-project/src/common/constants.js
+++ vite-project/src/common/constants.js
@@ -1,3 +1,8 @@
 export const GITHUB_LINK = 'https://www.github.com';
 export const GMAIL_LINK = 'https://www.gmail.com';
 export const FACEBOOK_LINK = 'https://www.facebook.com';
+
+export const POST_TITLE_MIN_LENGTH = 16;
+export const POST_TITLE_MAX_LENGTH = 64;
+export const POST_CONTENT_MIN_LENGTH = 32;
+export const POST_CONTENT_MAX_LENGTH = 8196;
--- vite-project/src/views/CreatePost/CreatePost.jsx
+++ vite-project/src/views/CreatePost/CreatePost.jsx
@@ -1,15 +1,21 @@
 import { useContext, useState } from "react";
 import AppContext from "../../context/AppContext";
 import { addPost } from "../../services/posts.service";
-import { Card, Row, Col, Button } from "react-bootstrap";
-import './CreatePost.css';
+import { Card, Row, Button } from "react-bootstrap";
+import "./CreatePost.css";
 import Loader from "../../components/Loader/Loader";
+import {
+  POST_CONTENT_MAX_LENGTH,
+  POST_CONTENT_MIN_LENGTH,
+  POST_TITLE_MAX_LENGTH,
+  POST_TITLE_MIN_LENGTH,
+} from "../../common/constants";
 
 export default function CreatePost() {
   const [post, setPost] = useState({
-    title: '',
-    content: '',
-    comments: []
+    title: "",
+    content: "",
+    comments: [],
   });
   const { user, userData } = useContext(AppContext);
   const [loading, setLoading] = useState(false);
@@ -18,57 +24,86 @@ export default function CreatePost() {
     setPost({
       ...post,
       [key]: value,
-    })
-  }
+    });
+  };
 
   const createPost = async () => {
     setLoading(true);
 
     if (!userData) {
-      return alert('You must be signed in to create a post');
+      return alert("You must be signed in to create a post");
     }
 
-    if (post.content.length < 5) {
-      return alert('Content must be at least 5 characters long');
+    if (
+      post.title.length < POST_TITLE_MIN_LENGTH ||
+      post.content.length > POST_TITLE_MAX_LENGTH
+    ) {
+      alert("The title must be 16-64 characters long!");
+      return setLoading(false);
+    }
+
+    if (
+      post.content.length < POST_CONTENT_MIN_LENGTH ||
+      post.content.length > POST_CONTENT_MAX_LENGTH
+    ) {
+      alert("The content must contain 32-8196 characters!");
+      return setLoading(false);
     }
 
     await addPost(userData.handle, post.title, post.content, post.comments);
 
     setPost({
-      title: '',
-      content: '',
-      comments: []
+      title: "",
+      content: "",
+      comments: [],
     });
 
     setLoading(false);
   };
 
   if (loading) {
-    return <Loader />
+    return <Loader />;
   }
 
   return (
     <Card className="px-5 py-3">
       <Card.Title>
-        {!user ? <h1>Sign in to create posts</h1> : <h1 className="mb-3">Create a new post</h1>}
+        {!user ? (
+          <h1>Sign in to create posts</h1>
+        ) : (
+          <h1 className="mb-3">Create a new post</h1>
+        )}
       </Card.Title>
       <Row className="mt-1 mb-3">
-        <label htmlFor="post-title" className="fs-5 fw-light">Title:</label>
-        <input type="text" name="post-title" value={post.title} onChange={e => updatePost(e.target.value, 'title')} className="form-control" />
+        <label htmlFor="post-title" className="fs-5 fw-light">
+          Title:
+        </label>
+        <input
+          type="text"
+          name="post-title"
+          value={post.title}
+          onChange={(e) => updatePost(e.target.value, "title")}
+          className="form-control"
+        />
       </Row>
       <Row className="mt-1 mb-3">
-        <label htmlFor="post-content" className="fs-5 fw-light">Content:</label>
+        <label htmlFor="post-content" className="fs-5 fw-light">
+          Content:
+        </label>
         <textarea
           className="form-control"
           name="post-content"
           id="post-content"
           cols="30"
           rows="10"
-          value={post.content} onChange={e => updatePost(e.target.value, 'content')}
+          value={post.content}
+          onChange={(e) => updatePost(e.target.value, "content")}
         ></textarea>
       </Row>
       <Row>
-        <Button className="create-post-button mt-3" onClick={createPost}>Create Post</Button>
+        <Button className="create-post-button mt-3" onClick={createPost}>
+          Create Post
+        </Button>
       </Row>
     </Card>
   );
