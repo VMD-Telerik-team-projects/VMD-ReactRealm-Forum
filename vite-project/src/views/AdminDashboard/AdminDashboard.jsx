0dd16a19aeadfa92fc2592bf379eb19727699962
Vladislava-Ivanova <ivanovavladislavav@gmail.com>
2024-05-08 13:33:55 +0300

    added the search bar component in the admin dashboard & added a filter users functionality

--- vite-project/src/views/AdminDashboard/AdminDashboard.jsx
+++ vite-project/src/views/AdminDashboard/AdminDashboard.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 import { getAllUsers } from "../../services/admin.service"
 import Loader from '../../components/Loader/Loader';
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 export default function AdminDashboard() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -25,12 +27,22 @@ export default function AdminDashboard() {
     return <Loader />
   }
 
+  const handleSearchChange = (search) => {
+    setSearchTerm(search);
+  };
+
+  const filteredUsers = users.filter(user => 
+    (user.handle?.toLowerCase().includes(searchTerm.toLowerCase())) ||
+    (user.email?.toLowerCase().includes(searchTerm.toLowerCase()))
+  );
+
   return (
     <>
       <h1 className='my-4'>Admin Dashboard</h1>
       <h3 className='mb-4'>Registered users: </h3>
+      <SearchBar value={searchTerm} onChange={handleSearchChange} />
       <div className='bg-white'>
-        {users.map(user => {
+        {filteredUsers.map(user => {
           return (
             <div key={user.uid}>
               <h1>{user.handle}</h1>
