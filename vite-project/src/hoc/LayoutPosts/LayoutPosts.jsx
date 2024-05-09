7fc2c253640eeb22533926470aca734207012b7c
deyantomov <deyan.tomov.a58@learn.telerikacademy.com>
2024-05-09 11:54:57 +0300

    Conditional rendering for posts layout

--- vite-project/src/hoc/LayoutPosts/LayoutPosts.jsx
+++ vite-project/src/hoc/LayoutPosts/LayoutPosts.jsx
@@ -1,29 +1,47 @@
 import Navigation from "../../components/Navigation/Navigation";
 import Footer from "../../components/Footer/Footer";
 import Sidebar from "../../components/Sidebar/Sidebar";
+import { useContext } from "react";
+import AppContext from "../../context/AppContext";
 import { Container, Row, Col } from "react-bootstrap";
 import "../Layout.css";
 import "../../index.css";
 import PropTypes from "prop-types";
 
 /**
- * 
- * @param {{children: React.ReactNode}} props 
+ *
+ * @param {{children: React.ReactNode}} props
  * @returns {JSX.Element}
  */
 export default function LayoutPosts({ children }) {
+  const { user } = useContext(AppContext);
+
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navigation />
       <main className="d-flex flex-row h-full w-full flex-grow body-bg m-0">
         <Container fluid className="p-0 m-0">
           <Row className="p-0 w-100 m-0">
-            <Col xs={1} className="m-0 p-0 h-full bg-dark">
-              <Sidebar />
-            </Col>
-            <Col xs={11} className="d-flex flex-column justify-content-center align-items-center m-0 p-0 w-full gap-4">
-              {children}
-            </Col>
+            {user ? (
+              <>
+                <Col xs={1} className="m-0 p-0 h-full bg-dark">
+                  <Sidebar />
+                </Col>
+                <Col
+                  xs={11}
+                  className="d-flex flex-column justify-content-center align-items-center m-0 p-0 w-full gap-4"
+                >
+                  {children}
+                </Col>
+              </>
+            ) : (
+              <Col
+                xs={11}
+                className="d-flex flex-column justify-content-center align-items-center m-0 p-0 w-full gap-4"
+              >
+                {children}
+              </Col>
+            )}
           </Row>
         </Container>
       </main>
@@ -34,4 +52,4 @@ export default function LayoutPosts({ children }) {
 
 LayoutPosts.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
